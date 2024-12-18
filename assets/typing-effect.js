5891159c452bec65fbb368eb8a77665db86368a4
Pranav Rajaram <prajaram@ucsd.edu>
2024-12-18 14:25:04 -0500

    trying

--- assets/typing-effect.js
+++ assets/typing-effect.js
@@ -3,11 +3,17 @@ document.addEventListener('DOMContentLoaded', function () {
     const titleElement = document.getElementById('animated-title');
     let i = 0;
 
+    // Start in light mode
+    document.body.classList.add('light-mode');
+
     function typeEffect() {
         if (i < text.length) {
             titleElement.innerHTML += text.charAt(i);
             i++;
             setTimeout(typeEffect, 100); // Adjust typing speed here
+        } else {
+            // Typing animation complete, remove light mode
+            document.body.classList.remove('light-mode');
         }
     }
 
